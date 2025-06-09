a6dd01729b41b71d5977fc14d7387f51e438c2d8
jonas baireuther <jonas.baireuther@gmx.de>
2025-06-09 20:12:48 +0200

    ci/cd fix router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,19 @@
-import { RouteObject } from "react-router";
+import { Navigate, RouteObject } from "react-router";
 import App from "./App";
 import { WelcomePage } from "./pages/welcome-page";
 
 export const routes: RouteObject[] = [
     {
         index: true,
-        element: <WelcomePage />
+        element: <Navigate to={'knister-app'} />,
     },
     {
-        path: 'game',
+        path: 'knister-app',
+        element: <WelcomePage />
+    },
+
+    {
+        path: 'knister-app/game',
         element: <App />
     }
 ]
